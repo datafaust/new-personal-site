4d68161236080d18af6af6a6964c92dc0cab8007
datafaust <faustolopez110@gmail.com>
2021-06-23 15:11:27 -0400

    added more files

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Row, Col, Image, Container } from 'react-bootstrap';
 
 import classes from './about.module.css';
+
+/**IMAGE LOADING */
 import gmail from '../assets/gmail.png';
+import github from '../assets/github.png';
+import cv from '../assets/curriculum-vitae.png';
+import linkedin from '../assets/linkedin.png';
 
 
 
 const About = props => {
     return (
-        <Row className={classes.main} id='about'>
+        <Container className={classes.main} id='about'>
+            
+            {/*TITLE*/}
+            <Row>
             <Col>
-                {/*TITLE*/}
                 <div className={classes.title}>About Me</div>
-                <div className={classes.summary}>blah blah blah blah</div>
+            </Col>
+            </Row>
+            
+            {/**SUMMARY */}
+            <Row>
+            <Col>
+                <paragraph className={classes.summary}>{'I am a creative and resourceful data engineer and software developer. Currently, I am the Director of Data Engineering and Analytics at the Taxi & Limousine Commission of the New York City. My work involves developing and automating big data processes, building and interacting with databases, creating apps and dashboards for TLC and public, as well as conducting high level research to empower impactful legislation in the area of city transportation. I am also a co-founder of VeryCool Studio - an app development, data analysis, and rapid prototyping company. In my spare time I work on data projects, repair motorcycles and make movies.'}</paragraph>
+            </Col>
+            </Row>
+            <br/>
+            {/**PANEL */}
+            <Row>
+                <Col>
                 <div className={classes.panel}>
                     <Image className={classes.icon} src={gmail} height={40} width={40} />
-                    <Image className={classes.icon}  src={gmail} height={40} width={40} />
-                    <Image className={classes.icon}  src={gmail} height={40} width={40} />
-                    <Image className={classes.icon}  src={gmail} height={40} width={40} />
+                    <Image className={classes.icon}  src={linkedin} height={40} width={40} />
+                    <Image className={classes.icon}  src={github} height={40} width={40} />
+                    <Image className={classes.icon}  src={cv} height={40} width={40} />
                 </div>
-            </Col>
-        </Row>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
